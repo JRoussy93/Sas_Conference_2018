4a9c1a565096b1dc855728aadd72ce9b93ddf9cf
Anghelos Coulon <anghelos@users.noreply.github.com>
2017-11-12 23:55:49 -0500

    Javascript scroller basis
    I've added a lot of stuff that might be removed later (we might not use it)
    
    little animation test, still needs a lot of work.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /* eslint-env browser */ //Removes annoying errors in ESLint (Brackets)
 
 /*------- VARIABLES --------*/
@@ -14,7 +13,9 @@ var latestKnownScrollY = 0;
 //Is the animation already updating?
 var ticking = false;
 
-
+//Array containing all the shape IDs
+var shapes = [getID('square1holder')];
+var initialpos
 
 
 
@@ -53,6 +54,36 @@ var transformProp = window.transformProp || (function () {
 /*------Main functions-------------*/
 
 
+//Gets element's rotation angle. (source: CSS Tricks)
+//Keeping this to potentially pause and restart the shapes' rotating animation.
+function getAngle(el) {
+    var st = window.getComputedStyle(el, null);
+    var tr = st.getPropertyValue("-webkit-transform") ||
+        st.getPropertyValue("-moz-transform") ||
+        st.getPropertyValue("-ms-transform") ||
+        st.getPropertyValue("-o-transform") ||
+        st.getPropertyValue("transform") ||
+        "FAIL";
+
+
+    // rotation matrix - http://en.wikipedia.org/wiki/Rotation_matrix
+
+    var values = tr.split('(')[1].split(')')[0].split(',');
+    var a = values[0];
+    var b = values[1];
+    var c = values[2];
+    var d = values[3];
+
+    var scale = Math.sqrt(a * a + b * b);
+
+
+    // arc sin, convert from radians to degrees, round
+    var sin = b / scale;
+    var angle = Math.round(Math.atan2(b, a) * (180 / Math.PI) * 100) / 100; //Added the (*100)/100 to keep some decimals 
+
+    return angle;
+}
+
 //Sets the latestKnownScrollY variable to the latest scroll position, then calls for animation
 function onScroll() {
     latestKnownScrollY = window.pageYOffset; // using pageYOffset because scrollY is not supported by IE
@@ -61,8 +92,8 @@ function onScroll() {
 
 
 //Checks to see if an animation request is already ongoing, otherwise requests a frame update
-function requestTick(){
-    if(!ticking){
+function requestTick() {
+    if (!ticking) {
         loop(update);
     }
     ticking = true;
@@ -70,13 +101,23 @@ function requestTick(){
 
 //The main function. Moves stuff around according to the scrolling position
 function update() {
-    // reset the tick so we can
-	// capture the next onScroll
+    // reset the tick so we can capture the next onScroll
     ticking = false;
+
+    //var currentScrollY = latestKnownScrollY;
+    var windowHeight = window.innerHeight;
     
-    var currentScrollY = latestKnownScrollY;
+    //selects shapes one by one and does stuff to them or not
+    for(var i=0; i<shapes.length; i++){
+        var shape = shapes[i];
+        var rect = shape.getBoundingClientRect(); // gets the element's position in the window
+        var top = rect.top; //gets element's distance from top of window
+        
+        if(top <= windowHeight && top >= windowHeight / 3){
+            shape.style.left = top - windowHeight/3 + 'px';
+        }
+    }
     
-    //getID('square1').innerHTML = currentScrollY;
 }
 
 
