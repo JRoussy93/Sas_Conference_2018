9c40c2168201e57ac3fcaa99f32cedfbee20f4bd
Anghelos Coulon <anghelos@users.noreply.github.com>
2017-11-18 18:00:16 -0500

    Added scroll animation to splash page

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@
 /*------- VARIABLES --------*/
 
 //Get Element by ID shortcut
-//var getID = function (id) {
-//    return document.getElementById(id);
-//};
+var getID = function (id) {
+    return document.getElementById(id);
+};
 
 // window scroll position
-//var latestKnownScrollY = 0;
+var latestKnownScrollY = 0;
 
 //Is the animation already updating?
 var ticking = false;
@@ -55,20 +55,25 @@ var loop = window.requestAnimationFrame ||
 
 /* ----TAKEN FROM RELLAX, adds vendor prefixes)*/
 // check which transform property to use 
-//var transformProp = window.transformProp || (function () {
-//    var testEl = document.createElement('div');
-//    if (testEl.style.transform == null) {
-//        var vendors = ['Webkit', 'Moz', 'ms'];
-//        for (var vendor in vendors) {
-//            if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
-//                return vendors[vendor] + 'Transform';
-//            }
-//        }
-//    }
-//    return 'transform';
-//})();
+var transformProp = window.transformProp || (function () {
+    var testEl = document.createElement('div');
+    if (testEl.style.transform == null) {
+        var vendors = ['Webkit', 'Moz', 'ms'];
+        for (var vendor in vendors) {
+            if (testEl.style[vendors[vendor] + 'Transform'] !== undefined) {
+                return vendors[vendor] + 'Transform';
+            }
+        }
+    }
+    return 'transform';
+})();
 
 
+//Easing function
+// t: current time, b: begInnIng value, c: change In value, d: duration
+function easeInQuad(t, b, c, d) {
+    return c * (t /= d) * t + b;
+}
 
 
 /*------Main functions-------------*/
@@ -82,7 +87,7 @@ function initializeAnim() {
 
 //Sets the latestKnownScrollY variable to the latest scroll position, then calls for animation
 function onScroll() {
-    //latestKnownScrollY = window.pageYOffset; // using pageYOffset because scrollY is not supported by IE
+    latestKnownScrollY = window.pageYOffset; // using pageYOffset because scrollY is not supported by IE
     requestTick();
 }
 
@@ -100,19 +105,28 @@ function update() {
     // reset the tick so we can capture the next onScroll
     ticking = false;
 
-    //var currentScrollY = latestKnownScrollY;
+    var currentScrollY = latestKnownScrollY;
     var windowHeight = window.innerHeight;
+
+    //Animates splash shapes
+    if (getID('splash')) { //if the page has a splash screen
+        if (currentScrollY <= windowHeight) {
+            getID('splash_circle').style[transformProp] = 'translate(-' + easeInQuad(currentScrollY, 0, 100, windowHeight) + '%, '+ easeInQuad(currentScrollY, 0, 50, windowHeight) +'%)';
+            
+            getID('splash_triangle').style[transformProp] = 'translate(' + easeInQuad(currentScrollY, 0, 100, windowHeight) + '%, '+ easeInQuad(currentScrollY, 0, 70, windowHeight) +'%) rotate('+ easeInQuad(currentScrollY, 10, 50, windowHeight) +'deg)'
+        }
+    }
 
     //selects elements one by one and does stuff to them or not
     for (var i = 0; i < objects.length; i++) {
         var object = objects[i].node;
         var rect = object.getBoundingClientRect(); // gets the element's position in the window
         var top = rect.top; //gets element's distance from top of window
-        
-        if (top <= windowHeight * 3/4 && top > 0) {
+
+        if (top <= windowHeight * 3 / 4 && top > 0) {
             object.classList.remove('out');
             objects[i].visible = true;
-        } else if (objects[i].visible && top > windowHeight * 3/4){
+        } else if (objects[i].visible && top > windowHeight * 3 / 4) {
             object.classList.add('out');
             objects[i].visible = false;
         }
